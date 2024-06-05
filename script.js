8e9b2cff63cc6d9cd9e415dff33bf348ff38ce9a
Sam <samya.hussain@hotmail.fr>
2024-06-05 15:29:36 +0200

    Adding colors

--- script.js
+++ script.js
@@ -1,74 +1,101 @@
-const game = document.getElementById('game');
-const cursor = document.getElementById('cursor');
-const colorChoice = document.getElementById('colorchoice');
-game.width = 1200
-game.height = 600
+const game = document.getElementById("game");
+const cursor = document.getElementById("cursor");
+const colorChoice = document.getElementById("colorchoice");
+game.width = window.innerWidth;
+game.height = window.innerHeight;
 
 const gridCellSize = 10;
 
+const colorList = [
+  "#000000",
+  "#FF0000",
+  "#ff6f00",
+  "#FFF500",
+  "#00FF57",
+  "#00FFFF",
+  "#0500FF",
+  "#AD00FF",
+  "#FFFFFF",
+];
+let currentColorChoice = colorList[1];
 
-const colorList = ["#FF0000","#FFF500", "#00FF57", "#00FFFF", "#0500FF", "#AD00FF", ];
-let currentColorChoice = colorList[4]
+colorList.forEach((color) => {
+  const colorItem = document.createElement("div");
+  colorItem.style.border = "1px solid black";
+  colorItem.style.backgroundColor = color;
+  colorChoice.appendChild(colorItem);
 
-colorList.forEach(color => {
-    const colorItem = document.createElement('div');
-    colorItem.style.backgroundColor = color;
-    colorChoice.appendChild(colorItem);
+  colorItem.addEventListener("click", () => {
+    currentColorChoice = color;
 
-    colorItem.addEventListener('click', () => { 
-        currentColorChoice = color;
-        colorItem.innerHTML = `<i class="fa-solid fa-check"></i>`
-
-        setTimeout(() => {
-            colorItem.innerHTML = "";
-        }, 1000);
-    } 
-    )
-}
-)
-
-
-const context = game.getContext('2d');
-const gridContext = game.getContext('2d');
-
-function addPixelIntoGame(){
-    const x = cursor.offsetLeft
-    const y = cursor.offsetTop - game.offsetTop;
-
-    context.beginPath()
-context.fillStyle = currentColorChoice;
-context.fillRect(x, y, gridCellSize, gridCellSize);
-
-
-}
-
-cursor.addEventListener('click', function(event) {
-    addPixelIntoGame();
+    setTimeout(() => {
+      colorItem.innerHTML = "";
+    }, 1000);
+  });
 });
 
-function drawGrids(context, width, height, cellWidth, cellHeight) {
-    context.beginPath();
-    context.strokeStyle = '#ccc';
-    for (let i = 0; i < width; i += cellWidth) {
-        context.moveTo(i + cellWidth, 0);
-        context.lineTo(i + cellWidth, height);
-    }
-    for (let i = 0; i < height; i += cellWidth) {
-        context.moveTo(0, i + cellHeight, 0);
-        context.lineTo(width, i + cellHeight);
-    }
-    context.stroke();
+const context = game.getContext("2d");
+
+let scale = 1;
+let panning = false;
+let start = { x: 0, y: 0 };
+let panX = 0;
+let panY = 0;
+
+function drawGrids() {
+  context.clearRect(0, 0, game.width, game.height);
+  context.save();
+  context.translate(panX, panY);
+  context.scale(scale, scale);
+
+  context.beginPath();
+  context.strokeStyle = "#ccc";
+  for (let i = 0; i < game.width / scale; i += gridCellSize) {
+    context.moveTo(i, 0);
+    context.lineTo(i, game.height / scale);
+  }
+  for (let j = 0; j < game.height / scale; j += gridCellSize) {
+    context.moveTo(0, j);
+    context.lineTo(game.width / scale, j);
+  }
+  context.stroke();
+  context.restore();
 }
-drawGrids(gridContext, game.width, game.height, gridCellSize, gridCellSize);
 
-game.addEventListener('mousemove', function(event) {
-    const cursorLeft = cursor.style.left = event.clientX - (cursor.offsetWidth/2)
-    const cursorTop = cursor.style.top = event.clientY -(cursor.offsetHeight/2)
+function addPixelIntoGame() {
+  const rect = game.getBoundingClientRect();
+  const x =
+    Math.floor((cursor.offsetLeft - rect.left - panX) / scale / gridCellSize) *
+    gridCellSize;
+  const y =
+    Math.floor((cursor.offsetTop - rect.top - panY) / scale / gridCellSize) *
+    gridCellSize;
 
-    cursor.style.left = Math.floor(cursorLeft / gridCellSize)*gridCellSize + "px";
-    cursor.style.top = Math.floor(cursorTop / gridCellSize)*gridCellSize + "px"; 
+  context.save();
+  context.translate(panX, panY);
+  context.scale(scale, scale);
+  context.fillStyle = currentColorChoice;
+  context.fillRect(x, y, gridCellSize, gridCellSize);
+  context.restore();
+}
+
+cursor.addEventListener("click", function () {
+  addPixelIntoGame();
 });
 
-game.addEventListener('click', function() {
-    addPixelIntoGame();
-}); 
+game.addEventListener("mousemove", function (event) {
+  const rect = game.getBoundingClientRect();
+  const cursorLeft = event.clientX - rect.left;
+  const cursorTop = event.clientY - rect.top;
+
+  cursor.style.left =
+    Math.floor(cursorLeft / scale / gridCellSize) * gridCellSize * scale +
+    panX +
+    "px";
+  cursor.style.top =
+    Math.floor(cursorTop / scale / gridCellSize) * gridCellSize * scale +
+    panY +
+    "px";
+  cursor.style.backgroundColor = currentColorChoice;
+});
+drawGrids();
